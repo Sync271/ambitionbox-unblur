17b95bfee4f7a35a331498093d4e705141677398
Abhishek K M <67158080+Sync271@users.noreply.github.com>
2024-06-22 02:15:12 +0530

    feat(salaries): remove "unlock" overlay

--- scripts/salary.js
+++ scripts/salary.js
@@ -1,10 +1,26 @@
-const elements = document.querySelectorAll("[is-locked]");
 const attributePattern = /^data-.*/;
-elements?.forEach((element) => {
-	Object.values(element.attributes).forEach((attribute) => {
-		if (attributePattern.test(attribute.name)) {
-			element.removeAttribute(attribute.name);
-		}
+const unBlurFn = () => {
+	const elements = document.querySelectorAll("[is-locked]");
+	elements?.forEach((element) => {
+		Object.values(element.attributes).forEach((attribute) => {
+			if (attributePattern.test(attribute.name)) {
+				element.removeAttribute(attribute.name);
+			}
+		});
+		element.style.filter = "";
 	});
-	element.style.filter = "";
-});
+};
+
+const removeOverlay = () => {
+	const elements = document.querySelectorAll(".overlay");
+	elements?.forEach((element) => {
+		element.remove();
+	});
+};
+
+const main = () => {
+	unBlurFn();
+	removeOverlay();
+};
+
+setInterval(main, 1000);
